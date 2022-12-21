a4e0b1fb82bc37cf3b398f9620d1627b22a34101
markwinap <markwinap@gmail.com>
2022-12-21 11:58:02 -0600

    updated error responses

--- src/services/yahoo-service.ts
+++ src/services/yahoo-service.ts
@@ -14,28 +14,30 @@ const forbiddenFetch = async (symbol: string) => {
     try {
         const response = await forbidden(symbol);
         const { status } = response;
+        if (status !== 200) {
+            throw new Error(status.toString());
+        }
         return status;
     } catch (error) {
         console.error(error);
     }
 }
 const unauthorizedFetch = async (symbol: string) => {
-    try {
-        const response = await unauthorized(symbol);
-        const { status } = response;
-        return status;
-    } catch (error) {
-        console.error(error);
+    const response = await unauthorized(symbol);
+    const { status } = response;
+    if (status !== 200) {
+        throw new Error(status.toString());
     }
+    return status;
 }
 const undefinedFetch = async (symbol: string) => {
-    try{
+    try {
         const response = await sucess(symbol);
         // @ts-ignore
         const { test } = response;
         return test;
     }
-    catch(error){
+    catch (error) {
         console.error(error);
     }
 }
@@ -43,18 +45,18 @@ const timeoutFetch = async (symbol: string) => {
     return fetchWithTimeout(symbol);
 }
 const throwError = async (symbol: string) => {
-    try{
+    try {
         throw new Error('Throw Error Service');
     }
-    catch(error){
+    catch (error) {
         console.log(error)
     }
 }
 const throwErrorAPIService = async (symbol: string) => {
-    try{
+    try {
         throwErrorAPI()
     }
-    catch(error){
+    catch (error) {
         console.log(error)
     }
 }
@@ -64,8 +66,8 @@ const randomNumber = (min: number, max: number) => {
 const runOutOfMemory = async (symbol: string) => {
     try {
         return new Promise((resolve, reject) => {
-            let temp : any = [];
-            for(;;){
+            let temp: any = [];
+            for (; ;) {
                 const _temp = [...temp];
                 _temp.push(randomNumber(0, Number.MAX_SAFE_INTEGER));
                 temp = _temp;
